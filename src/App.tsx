e64d3dd493a03d97f2fc4fcbf2d7f4ca8bd6c9b2
radiylon <radiylon@gmail.com>
2025-04-30 14:59:01 -0700

    Add hot-reloading

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+
+const getSiteInfo = async () => {
+  const siteInfo = await webflow.getSiteInfo();
+  console.log(siteInfo);
+  return siteInfo;
+}
+
+type SiteInfo = Awaited<ReturnType<typeof getSiteInfo>>;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [siteInfo, setSiteInfo] = useState<SiteInfo>();
+
+  useEffect(() => {
+    getSiteInfo().then((info) => setSiteInfo(info));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1 className='text-4xl font-bold'>Hello World</h1>
+      <p>{siteInfo?.siteName}</p>
+      <p>{siteInfo?.siteId}</p>
+      <p>{siteInfo?.domains[0].url}</p>
+    </div>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import wfHotReload from "@xatom/wf-app-hot-reload";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(), 
-    tailwindcss()
+    tailwindcss(),
+    wfHotReload()
   ],
   server: {
     port: 1337
